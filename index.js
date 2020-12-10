13d6a390a2c072921559bd31fd469353ba7c3096
TMThanh7217 <onthidaihocvatly12@gmail.com>
2020-12-10 10:41:52 +0700

    Deploy

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', (req, res)=>{
+    res.sendFile('/public/index.html');
+});
+
+app.listen(port,() => {
+    console.log('Server is listening at http://localhost:${port}');
+});
