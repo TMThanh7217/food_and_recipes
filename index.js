4a5d426d08ca23a7bbe2f91796f5344c48c80453
TMThanh7217 <onthidaihocvatly12@gmail.com>
2020-12-10 18:23:32 +0700

    a

--- index.js
+++ index.js
@@ -29,6 +29,11 @@ app.get('/', (req, res)=>{
     res.render('index', {groupID : 11});
 });
 
+app.get('/index', (req, res)=>{
+    res.locals.title = 'Group 11';
+    res.render('index', {groupID : 11});
+});
+
 app.get('/recipes', (req, res)=>{
     res.locals.title = '18127130';
     models.Recipe
@@ -47,23 +52,49 @@ app.get('/recipes', (req, res)=>{
 
 app.get('/featured', function(req, res){
     res.locals.title = '18127217';
+    var recipe_data;
+    var ingredient_data;
+    var direction_data;
     models.Recipe
     .findAll({
         raw: true
     }) 
     .then(function(data) {
-        var recipe = data;
+        recipe_data = data;
         console.log(typeof(data));
-        res.render('recipes', {recipes: recipe, mssv:18127217, name:'Trinh Minh Thanh', mail:'18127217@student.hcmus.edu.vn'});
+    })
+    .catch(function(err) {
+        res.json(err);
+    });
+    models.Ingredient
+    .findAll({
+        raw: true
+    }) 
+    .then(function(data) {
+        ingredient_data = data;
+        console.log(typeof(data));
+    })
+    .catch(function(err) {
+        res.json(err);
+    });
+
+    models.Direction
+    .findAll({
+        raw: true
+    }) 
+    .then(function(data) {
+        direction_data = data;
+        console.log(typeof(data));
     })
     .catch(function(err) {
         res.json(err);
     })
+    res.render('featured', {products: {recipe_data, ingredient_data, direction_data}, mssv:18127217, name:'Trinh Minh Thanh', mail:'18127217@student.hcmus.edu.vn'});
 });
 
 app.get('/sync', function(req, res) {
     models.sequelize.sync().then(function() {
-        res.send('Ahoy, Database sync completed');
+        res.send('Database sync completed');
     });
 });
 
